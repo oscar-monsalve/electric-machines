fcff040c60f3c55d94bc9aa3bb1f6fc73c00257a
oscar monsalve <oscarmonsalvec5@gmail.com>
2025-04-13 00:06:52 -0500

    update

--- synchronous_generator/parcial_2/01_resistive_load.py
+++ synchronous_generator/parcial_2/01_resistive_load.py
@@ -2,7 +2,6 @@ import helpers as h
 
 
 # Input data
-LOAD_TYPE:   str = "r"  # "r" or "c" or "l" for resistive, capacitive or inductive load type.
 RL_CEDULA: float = 175  # Últimos dos dígitos de la cédula para definir a la impedancia de carga
 ZL:      complex = RL_CEDULA + 0j
 EA:        float = 2400
@@ -11,8 +10,33 @@ XS:      complex = 144j
 
 
 def main() -> None:
-    power_factor_angle = h.power_factor_angle(ZL, LOAD_TYPE)
-    print(power_factor_angle)
+    zs_phasor = RA + XS
+    zs_magnitude = h.syncrhonous_impedance_magnitude(RA, XS)
+    req = h.equivalent_circuit_resistance(RA, RL_CEDULA)
+    xeq = h.equivalent_circuit_reactance(XS, ZL)
+    zeq = req + xeq
+    power_factor_angle = h.power_factor_angle(ZL)
+    ia_rec, ia_pol = h.armature_current(EA, zeq, power_factor_angle)
+    phase_voltage = h.phase_voltage(ia_pol[0], ZL)
+    line_voltage = h.line_voltage(phase_voltage)
+    p_in = h.input_power(line_voltage, ia_pol[0], power_factor_angle, RA)
+    ea_magnitud, ea_phase_angle = h.internal_generated_voltage_phasor(phase_voltage, ia_rec, zs_phasor)
+    v_ra_magnitude, v_ra_phase_angle = h.armature_resistance_voltage(ia_rec, RA)
+    v_xs_magnitude, v_xs_phase_angle = h.syncrhonous_reactance_voltage(ia_rec, XS)
+
+    print(f"a). Z_S   : {zs_magnitude} Ω")
+    print(f"b). R_eq  : {req} Ω")
+    print(f"    X_eq  : {xeq} Ω")
+    print(f"    Z_eq  : {zeq} Ω")
+    print(f"c). I_A   : {ia_pol[0]} A ∠{power_factor_angle:.2f}°")
+    print(f"d). V_phi : {phase_voltage:.2f} V")
+    print(f"    V_L   : {line_voltage:.2f} V")
+    print(f"e). P_in  : {p_in:.2f} W")
+    print("f). Phasor diagram values:")
+    print(f"         V_phi    : {phase_voltage:.2f} V ∠0°")
+    print(f"         E_A      : {ea_magnitud:.2f} V ∠{ea_phase_angle:.2f}°")
+    print(f"         I_A*R_A  : {v_ra_magnitude:.2f} V ∠{v_ra_phase_angle:.2f}°")
+    print(f"         I_A*jX_S : {v_xs_magnitude:.2f} V ∠{v_xs_phase_angle:.2f}°")
 
 
 if __name__ == "__main__":
--- synchronous_generator/parcial_2/helpers.py
+++ synchronous_generator/parcial_2/helpers.py
@@ -1,48 +1,71 @@
 import cmath
-from math import sqrt
-from math import radians
+from math import sqrt, radians, degrees, cos
 
 
-def syncrhonous_impedance() -> complex:
-    pass
+def syncrhonous_impedance_magnitude(armature_resistance: float, syncrhonous_reactance: complex, ) -> complex:
+    return sqrt(armature_resistance**2 + syncrhonous_reactance.imag**2)
 
 
-def equivalent_circuit_resistance() -> float:
-    pass
+def equivalent_circuit_resistance(armature_resistance: float, load_resistance: float, ) -> float:
+    return armature_resistance + load_resistance
 
 
-def equivalent_circuit_reactance() -> complex:
-    pass
+def equivalent_circuit_reactance(syncrhonous_reactance: float, load_reactance: complex) -> complex:
+    return complex(0, syncrhonous_reactance.imag + load_reactance.imag)
 
 
-def equivalent_circuit_impedance_magnitude() -> float:
-    pass
+def equivalent_circuit_impedance_magnitude(circuit_equivalent_resistance: float, circuit_equivalent_reactance: complex, ) -> float:
+    return sqrt(circuit_equivalent_resistance**2 + circuit_equivalent_reactance.imag**2)
 
 
-def power_factor_angle(load_impedance: complex, load_type: str) -> float:
-    if load_type not in ("r", "l", "c"):
-        raise TypeError("Incorrect load type. Insert 'r', 'l', or 'c' for resistive, inducive or capacitive load.")
-    elif load_type == "r":
-        return cmath.polar(load_impedance)[1]
-    elif load_type == "l":
-        pass
-    elif load_type == "c":
-        pass
+def power_factor_angle(load_impedance: complex) -> float:
+    if load_impedance.imag == 0:
+        return degrees(cmath.polar(load_impedance)[1])
+    elif load_impedance.imag > 0:
+        return degrees(cmath.polar(load_impedance)[1]) * -1
+    elif load_impedance.imag < 0:
+        return degrees(cmath.polar(load_impedance)[1]) * -1
 
 
-def armature_current(connection: str, load_current: float, nominal_voltage: float, pf_angle: float) -> complex:
-    """Returns the armature current I_A based on the load connection to the generator."""
+def armature_current(internal_generated_voltage: float, equivalent_impedance: complex,
+                     pf_angle: float) -> tuple[complex, tuple[float, float]]:
+    ia = internal_generated_voltage / sqrt(equivalent_impedance.real**2 + equivalent_impedance.imag**2)
+    i_a_rec = cmath.rect(ia, radians(pf_angle))
+    i_a_pol = cmath.polar(i_a_rec)
+    return i_a_rec, i_a_pol
 
-    if connection == "delta":
-        v_phi_delta = complex(nominal_voltage, 0)
-        i_a_delta = load_current / sqrt(3)
-        i_a_delta_rec = cmath.rect(i_a_delta, radians(pf_angle))
-        i_a_delta_pol = cmath.polar(i_a_delta_rec)
-        return i_a_delta_rec, i_a_delta_pol, v_phi_delta
 
-    if connection == "star":
-        v_phi_star = complex(nominal_voltage / sqrt(3), 0)
-        i_a_star = load_current
-        i_a_star_rec = cmath.rect(i_a_star, radians(pf_angle))
-        i_a_star_pol = cmath.polar(i_a_star_rec)
-        return i_a_star_rec, i_a_star_pol, v_phi_star
+def phase_voltage(armature_current_magnitude: float, load_impedance: float) -> float:
+    return armature_current_magnitude * sqrt(load_impedance.real**2 + load_impedance.imag**2)
+
+
+def line_voltage(phase_voltage: float) -> float:
+    return phase_voltage * sqrt(3)
+
+
+def input_power(line_voltage: float, armature_current: float, pf_angle: float, armature_resistance: float) -> float:
+    p_out = sqrt(3) * line_voltage * armature_current * cos(radians(pf_angle))
+    p_cu = 3 * armature_current**2 * armature_resistance
+    return p_out + p_cu
+
+
+def internal_generated_voltage_phasor(phase_voltage: float, armature_current: complex,
+                                      syncrhonous_reactance: complex) -> tuple[float, float]:
+    ea_rec = phase_voltage + armature_current * (syncrhonous_reactance)
+    ea_magnitud = cmath.polar(ea_rec)[0]
+    ea_phase_angle = degrees(cmath.polar(ea_rec)[1])
+    return ea_magnitud, ea_phase_angle
+
+
+def armature_resistance_voltage(armature_current: complex, armature_resistance: float) -> tuple[float, float]:
+    v_ra = armature_current * armature_resistance
+    v_ra_magnitud = cmath.polar(v_ra)[0]
+    v_ra_angle = degrees(cmath.polar(v_ra)[1])
+    return v_ra_magnitud, v_ra_angle
+
+
+def syncrhonous_reactance_voltage(armature_current: complex, syncrhonous_reactance: complex) -> tuple[float, float]:
+    v_xs = armature_current * syncrhonous_reactance
+    v_xs_magnitud = cmath.polar(v_xs)[0]
+    v_xs_angle = degrees(cmath.polar(v_xs)[1])
+    return v_xs_magnitud, v_xs_angle
