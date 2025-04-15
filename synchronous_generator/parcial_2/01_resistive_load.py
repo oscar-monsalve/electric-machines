92660ef4bc164c68a0b48e3ffc5c304b51bda161
oscar monsalve <oscarmonsalvec5@gmail.com>
2025-04-14 22:52:31 -0500

    feat: added phasor diagrams

--- synchronous_generator/parcial_2/01_resistive_load.py
+++ synchronous_generator/parcial_2/01_resistive_load.py
@@ -1,6 +1,6 @@
-from math import sin, cos, degrees
-import helpers as h
+from math import degrees
 import matplotlib.pyplot as plt
+import helpers as h
 
 
 # Input data
@@ -29,10 +29,10 @@ def main() -> None:
     v_xs_magnitude, v_xs_phase_angle = h.syncrhonous_reactance_voltage(ia_rec, XS)
     phasors = [
         (phase_voltage, 0),
-        (ia_pol[0], ia_pol[1]),
+        (ea_magnitud, ea_phase_angle),
+        (ia_pol[0], degrees(ia_pol[1])),
         (v_ra_magnitude, v_ra_phase_angle),
         (v_xs_magnitude, v_xs_phase_angle),
-        (ea_magnitud, ea_phase_angle),
     ]
     phasor_xy_coordinates = h.phasor_xy_coordinates(phasors)
 
@@ -47,14 +47,30 @@ def main() -> None:
     print("f). Phasor diagram values:")
     print(f"         V_phi    : {phase_voltage:.2f} V ∠0°")
     print(f"         E_A      : {ea_magnitud:.2f} V ∠{ea_phase_angle:.2f}°")
+    print(f"         I_A      : {ia_pol[0]:.2f} V ∠{ia_pol[1]:.2f}°")
     print(f"         I_A*R_A  : {v_ra_magnitude:.2f} V  ∠{v_ra_phase_angle:.2f}°")
     print(f"         I_A*jX_S : {v_xs_magnitude:.2f} V ∠{v_xs_phase_angle:.2f}°")
 
     v_phase_vec = phasor_xy_coordinates[0]
-    i_a_vec = phasor_xy_coordinates[1]
-    v_ra_vec = phasor_xy_coordinates[2]
-    v_xs_vec = phasor_xy_coordinates[3]
+    ea_vec = phasor_xy_coordinates[1]
+    ia_vec = phasor_xy_coordinates[2]
+    v_ra_vec = phasor_xy_coordinates[3]
     v_xs_vec = phasor_xy_coordinates[4]
+
+    fig, ax = plt.subplots(figsize=(10, 7))
+    ax.set_aspect("equal")
+
+    ax.quiver(0, 0, v_phase_vec[0], v_phase_vec[1], angles="xy", scale_units="xy", scale=1, color="blue", label=r"$V_{\phi}$")
+    ax.quiver(0, 0, ia_vec[0], ia_vec[1], angles="xy", scale_units="xy", scale=0.04, color="red", label=r"$I_A$ (not to scale)")
+    ax.quiver(0, 0, ea_vec[0], ea_vec[1], angles="xy", scale_units="xy", scale=1, color="purple", label=r"$E_A$")
+    ax.quiver(v_phase_vec[0], v_phase_vec[1], v_ra_vec[0], v_ra_vec[1], angles="xy", scale_units="xy", scale=1, color="green", label=r"$I_A R_A$")
+    ax.quiver(v_phase_vec[0]+v_ra_vec[0], v_phase_vec[1]+v_ra_vec[1], v_xs_vec[0], v_xs_vec[1], angles="xy", scale_units="xy", scale=1, color="black", label=r"$I_A\times jX_S$")
+
+    ax.legend(loc="upper left")
+    ax.grid(True)
+    ax.set_xlim(-100, max(ea_vec[0], v_phase_vec[0])+50)
+    ax.set_ylim(min(ia_vec[1], v_phase_vec[1])-50, max(ea_vec[1], v_phase_vec[1])+50)
+    plt.show()
 
 
 if __name__ == "__main__":
--- synchronous_generator/parcial_2/03_capacitive_load.py
+++ synchronous_generator/parcial_2/03_capacitive_load.py
@@ -1,3 +1,5 @@
+from math import degrees
+import matplotlib.pyplot as plt
 import helpers as h
 
 
@@ -25,6 +27,19 @@ def main() -> None:
     ea_magnitud, ea_phase_angle = h.internal_generated_voltage_phasor(phase_voltage, ia_rec, zs_phasor)
     v_ra_magnitude, v_ra_phase_angle = h.armature_resistance_voltage(ia_rec, RA)
     v_xs_magnitude, v_xs_phase_angle = h.syncrhonous_reactance_voltage(ia_rec, XS)
+    phasors = [
+        (phase_voltage, 0),
+        (ea_magnitud, ea_phase_angle),
+        (ia_pol[0], degrees(ia_pol[1])),
+        (v_ra_magnitude, v_ra_phase_angle),
+        (v_xs_magnitude, v_xs_phase_angle),
+    ]
+    phasor_xy_coordinates = h.phasor_xy_coordinates(phasors)
+    v_phase_vec = phasor_xy_coordinates[0]
+    ea_vec = phasor_xy_coordinates[1]
+    ia_vec = phasor_xy_coordinates[2]
+    v_ra_vec = phasor_xy_coordinates[3]
+    v_xs_vec = phasor_xy_coordinates[4]
 
     print(f"a). Z_S   : {zs_magnitude} Ω")
     print(f"b). R_eq  : {req} Ω")
@@ -39,6 +54,21 @@ def main() -> None:
     print(f"         E_A      : {ea_magnitud:.2f} V ∠{ea_phase_angle:.2f}°")
     print(f"         I_A*R_A  : {v_ra_magnitude:.2f} V  ∠{v_ra_phase_angle:.2f}°")
     print(f"         I_A*jX_S : {v_xs_magnitude:.2f} V ∠{v_xs_phase_angle:.2f}°")
+
+    fig, ax = plt.subplots(figsize=(10, 7))
+    ax.set_aspect("equal")
+
+    ax.quiver(0, 0, v_phase_vec[0], v_phase_vec[1], angles="xy", scale_units="xy", scale=1, color="blue", label=r"$V_{\phi}$")
+    ax.quiver(0, 0, ia_vec[0], ia_vec[1], angles="xy", scale_units="xy", scale=0.04, color="red", label=r"$I_A$ (not to scale)")
+    ax.quiver(0, 0, ea_vec[0], ea_vec[1], angles="xy", scale_units="xy", scale=1, color="purple", label=r"$E_A$")
+    ax.quiver(v_phase_vec[0], v_phase_vec[1], v_ra_vec[0], v_ra_vec[1], angles="xy", scale_units="xy", scale=1, color="green", label=r"$I_A R_A$")
+    ax.quiver(v_phase_vec[0]+v_ra_vec[0], v_phase_vec[1]+v_ra_vec[1], v_xs_vec[0], v_xs_vec[1], angles="xy", scale_units="xy", scale=1, color="black", label=r"$I_A\times jX_S$")
+
+    ax.legend(loc="upper left")
+    ax.grid(True)
+    ax.set_xlim(-100, max(ea_vec[0], v_phase_vec[0])+50)
+    ax.set_ylim(min(ia_vec[1], v_phase_vec[1])-50, max(ea_vec[1], v_phase_vec[1])+50)
+    plt.show()
 
 
 if __name__ == "__main__":
