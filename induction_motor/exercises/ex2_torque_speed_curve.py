f3bd2daf2ee8f4b03bc21242908c952ebc0f948f
Oscar Monsalve <oscarmonsalvec5@gmail.com>
2025-08-28 16:05:19 -0500

    updated induction_motor/exercises/ex2 plotting

--- induction_motor/exercises/ex2_torque_speed_curve.py
+++ induction_motor/exercises/ex2_torque_speed_curve.py
@@ -41,7 +41,8 @@ def main() -> None:
     torque_at_startup_at_r2_doubled = helpers_ex2.torque_induced(v_th, r_th, x_th, r2_doubled, X2, sync_velocity_radsec, 1)  # s=1 at startup
 
     # solution for d)
-    s_values = linspace(0, 50)
+    solution_space = 200
+    s_values = linspace(0, solution_space, num=solution_space)
     s_values[0] = 0.001  # replacing the first element of s_values from 0 to 0.0001 to avoid zero division
 
     # Init lists
@@ -50,9 +51,9 @@ def main() -> None:
     torque_list_doubled_r2 = []
 
     for i in s_values:
-        shaft_velocity = helpers_ex2.shaft_velocity(sync_velocity_rpm, i/50)
-        torque_r2 = helpers_ex2.torque_induced(v_th, r_th, x_th, R2, X2, sync_velocity_radsec, i/50)
-        torque_r2_doubled = helpers_ex2.torque_induced(v_th, r_th, x_th, r2_doubled, X2, sync_velocity_radsec, i/50)
+        shaft_velocity = helpers_ex2.shaft_velocity(sync_velocity_rpm, i/solution_space)
+        torque_r2 = helpers_ex2.torque_induced(v_th, r_th, x_th, R2, X2, sync_velocity_radsec, i/solution_space)
+        torque_r2_doubled = helpers_ex2.torque_induced(v_th, r_th, x_th, r2_doubled, X2, sync_velocity_radsec, i/solution_space)
         shaft_velocity_list.append(shaft_velocity)
         torque_list_r2.append(torque_r2)
         torque_list_doubled_r2.append(torque_r2_doubled)
@@ -60,8 +61,8 @@ def main() -> None:
     # Print results
     print("Resultados:\n")
 
-    print(f"V_TH: {v_th}")
-    print(f"Z_TH: {z_th}\n")
+    # print(f"V_TH: {v_th:.2f}")
+    # print(f"Z_TH: {z_th:.2f}\n")
 
     print(f"a). - Par máximo                  -> T_max: {torque_max:.2f} Nm.")
     print(f"    - Deslizamiento en par máximo -> s_max: {s_max:.4f}.")
@@ -69,7 +70,7 @@ def main() -> None:
 
     print(f"b). Par en el arranque -> T en arranque: {torque_at_startup:.2f} Nm.\n")
 
-    print(f"c). Si se duplica R2 (2R2: {r2_doubled} ohms), el deslizamiento se duplica, al cual se presenta el par máximo:")
+    print(f"c). Si se duplica R2 (2R2: {r2_doubled} ohms),el deslizamiento se duplica,\n    al cual se presenta el par máximo:")
     print(f"    - Deslizamiento duplicando R2          -> s_max @2R2: {s_max_at_r2_doubled:.4f}.")
     print(f"    - Velocidad del rotor con 2R2          -> N_m @2R2: {velocity_at_r2_doubled:.2f} rpm.")
     print(f"    - El torque máximo no depende de R2    -> T_max: {torque_max:.2f} Nm.")
@@ -77,7 +78,7 @@ def main() -> None:
 
     plt.plot(shaft_velocity_list, torque_list_r2, label=r"$R_2$ original")
     plt.plot(shaft_velocity_list, torque_list_doubled_r2, '-.', label=r"$R_2$ doble")
-    plt.plot(shaft_velocity_list, [torque_max]*50, '--', label=fr"$\tau_{{máx}}={torque_max:.2f}$ Nm", color='red')
+    plt.plot(shaft_velocity_list, [torque_max]*solution_space, '--', label=fr"$\tau_{{máx}}={torque_max:.2f}$ Nm", color='red')
     plt.xlabel(r"Velocidad angular $n_m$ (rpm)")
     plt.ylabel(r"Torque inducido $\tau_{ind}$ (Nm)")
     plt.title(r"Característica par-velocidad del motor")
